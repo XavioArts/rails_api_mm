df92a82d88387be31a0ed88b15f4b31463265d0b
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-29 14:49:53 -0700

    initial rails set up

---
+++ db/migrate/20211129214710_create_monsters.rb
@@ -0,0 +1,12 @@
+class CreateMonsters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :monsters do |t|
+      t.string :name
+      t.integer :life
+      t.float :cr
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
