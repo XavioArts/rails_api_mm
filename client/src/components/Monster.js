0757ad21accb012f98bd6ac3930c73c7e28d3746
Fabio Xavier <fabio.g.xavier33@gmail.com>
2021-11-29 16:38:53 -0700

    view all models working

--- client/src/components/Monster.js
+++ client/src/components/Monster.js
@@ -2,7 +2,14 @@ import React from "react";
 import MonForm from "./MonForm";
 
 const Monster = (props) => {
-
+    return (
+        <div>
+            <h3>{props.name}</h3>
+            <p>Type: {props.mon_type}</p>
+            <p>Health Points: {props.life}</p>
+            <p>Challenge Rating: {props.cr}</p>
+        </div>
+    )
 };
 
 export default Monster;
--- client/src/components/Monsters.js
+++ client/src/components/Monsters.js
@@ -3,9 +3,33 @@ import axios from "axios";
 import Monster from "./Monster";
 
 const Monsters = (props) => {
+
+    const [mons, setMons] = useState([]);
+
+    useEffect(()=> {
+        getMons();
+    }, []);
+
+    const getMons = async () => {
+        let result = await axios.get("/api/monsters");
+        setMons(result.data);
+    };
+
+    const renderMons = () => {
+        if (mons.length === 0) {
+            return (
+                <h3>No Entries</h3>
+            );
+        } else
+        return mons.map((mon) => {
+            return <Monster {...mon} key={mon.id} />;
+        });
+    };
+
     return (
         <div>
             <h2>All monsters:</h2>
+            {renderMons()}
         </div>
     )
 };
